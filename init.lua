e17cf8f9c894a1753dc178df1f53b4d9cb61cfc4
Bogdan Kurbetyev <bogdan.kurbetyev@gmail.com>
2025-05-21 14:10:57 +1000

    wip

--- init.lua
+++ init.lua
@@ -961,6 +961,23 @@ require('lazy').setup({
   },
 
   {
+    'github/copilot.vim',
+  },
+
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
+    },
+    -- build = 'make tiktoken', -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+
+  {
     'romgrk/barbar.nvim',
     dependencies = {
       'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
